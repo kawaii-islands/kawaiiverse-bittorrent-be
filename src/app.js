63f00d2f19650c312957d14087fb2ffb285d1a6e
canhtuan97 <tuantc@orai.io>
2022-07-14 16:24:31 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use(cors());
+app.use(cors("*"));
 // app.options('kawaii-islands.airight.io', cors());
 
 app.use(routes);

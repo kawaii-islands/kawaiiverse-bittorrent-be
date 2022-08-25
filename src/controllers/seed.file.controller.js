a773de96513b9099875b057db4730cc44938bf01
canhtuan97 <tuantc@orai.io>
2022-08-25 15:22:30 +0700

    debug2

--- src/controllers/seed.file.controller.js
+++ src/controllers/seed.file.controller.js
@@ -33,20 +33,20 @@ module.exports = {
                 });
             }
 
-            // let result = await addPending(req, magnetUrl, parseManet);
-            // if (typeof result == "string") {
-            //     return res.status(200).send({
-            //         status: 500, msg: result,
-            //     });
-            // }
-            // // await requestSeedService.requestSeed(magnetUrl);
-            // return res.status(200).send({
-            //     status: 200, msg: 'success', data: {
-            //         name: parseManet.name,
-            //         magnetId: magnetUrl,
-            //         infoHash: parseManet.infoHash,
-            //     },
-            // });
+            let result = await addPending(req, magnetUrl, parseManet);
+            if (typeof result == "string") {
+                return res.status(200).send({
+                    status: 500, msg: result,
+                });
+            }
+            // await requestSeedService.requestSeed(magnetUrl);
+            return res.status(200).send({
+                status: 200, msg: 'success', data: {
+                    name: parseManet.name,
+                    magnetId: magnetUrl,
+                    infoHash: parseManet.infoHash,
+                },
+            });
             return res.status(200).send({
                 status: 200, msg: "test",
             });
@@ -141,23 +141,23 @@ module.exports = {
 async function addPending(req, url, parseManet) {
     return new Promise((resolve, reject) => {
         client.add(url, {private: true}, async (torrent) => {
-            torrent.on("download", function (bytes) {
-                req.app.io.emit(`download/${torrent.infoHash}`, {
-                    downloadSpeed: torrent.downloadSpeed,
-                    progress: torrent.progress,
-                });
-            });
-            torrent.on('done', function () {
-                console.log(`done download file ${torrent.name} - magnetId - ${url}`);
-                const files = torrent.files;
-                files.forEach(async function (file) {
-                    await googleStorageService.uploadFile(`${file._torrent.path}/${file.name}`, `${parseManet.infoHash}/${file.name}`);
-                    await googleStorageService.uploadFileByContent(JSON.stringify({
-                        tracker: parseManet.announce,
-                    }), `${parseManet.infoHash}/info.json`);
-                });
-                req.app.io.emit(`seed-done/${torrent.infoHash}`, {msg: "success"});
-            });
+            // torrent.on("download", function (bytes) {
+            //     req.app.io.emit(`download/${torrent.infoHash}`, {
+            //         downloadSpeed: torrent.downloadSpeed,
+            //         progress: torrent.progress,
+            //     });
+            // });
+            // torrent.on('done', function () {
+            //     console.log(`done download file ${torrent.name} - magnetId - ${url}`);
+            //     const files = torrent.files;
+            //     files.forEach(async function (file) {
+            //         await googleStorageService.uploadFile(`${file._torrent.path}/${file.name}`, `${parseManet.infoHash}/${file.name}`);
+            //         await googleStorageService.uploadFileByContent(JSON.stringify({
+            //             tracker: parseManet.announce,
+            //         }), `${parseManet.infoHash}/info.json`);
+            //     });
+            //     req.app.io.emit(`seed-done/${torrent.infoHash}`, {msg: "success"});
+            // });
             resolve(torrent);
         });
     });

b8c5fb5607b29fab2a9f6f503b92db8096128f19
canhtuan <tuan.tc@orai.io>
2023-05-05 10:57:15 +0700

    Da

--- client1.js
+++ client1.js
@@ -2,7 +2,7 @@ import WebTorrent from "webtorrent-hybrid";
 
 const client = new WebTorrent();
 
-const torrentId = 'magnet:?xt=urn:btih:a656ddccefd0207e6a5c5ea04c29d40271c77102&dn=1GB&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337';
+const torrentId = 'magnet:?xt=urn:btih:c09006da56f8f0b90d6a7baf90f1ba723aff7d63&dn=150MB&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337';
 let start = Date.now();
 console.log("start", start);
 client.add(torrentId, {path: "/Users/admin/Desktop/This-pc/orai/kawaiiverse-bittorrent-be"}, (torrent) => {
@@ -12,5 +12,9 @@ client.add(torrentId, {path: "/Users/admin/Desktop/This-pc/orai/kawaiiverse-bitt
         console.log('download speed: ' + torrent.downloadSpeed);
         console.log('progress: ' + torrent.progress);
     });
-    console.log(`took ${Date.now() - start}`);
+    torrent.on('done', function (bytes) {
+        console.log('torrent finished downloading')
+        console.log(`took ${Date.now() - start}`);
+    });
+
 });
--- client2.js
+++ client2.js
@@ -1,28 +1,28 @@
 import WebTorrent from "webtorrent-hybrid";
 
-const client = new WebTorrent({
-    dht: false,
-});
-console.log("peerId", client.peerId);
-
-async function seedFile() {
-    let filePath = "/Users/admin/Desktop/This-pc/orai/kawaiiverse-bittorrent-be/1.png";
+async function seedFile1() {
+    const client = new WebTorrent();
+    console.log("peerId", client.peerId);
+    let filePath = "/Users/admin/Desktop/This-pc/orai/kawaiiverse-bittorrent-be/storage/150MB";
 
     client.seed(filePath,
-        {
-            private: true,
-            announce: ["wss://tracker.eueno.io", "https://tracker.eueno.io/announce", "http://localhost:8000/announce"],
-            urlList: ["https://storage.googleapis.com/data.eueno.io/test/1.png"],
-            getAnnounceOpts: () => {
-                return {
-                    "x-api-key": "ahihi",
-                };
-            },
-        },
         (torrent) => {
             console.log("torrent", torrent.infoHash);
         },
     );
 }
 
-seedFile();
+async function seedFile2() {
+    const client = new WebTorrent();
+    console.log("peerId", client.peerId);
+    let filePath = "/Users/admin/Desktop/This-pc/orai/kawaiiverse-bittorrent-be/storage/150MB";
+
+    client.seed(filePath,
+        (torrent) => {
+            console.log("torrent", torrent.infoHash);
+        },
+    );
+}
+
+seedFile1();
+seedFile2();
